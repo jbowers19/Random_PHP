9fd71d0bd3ae234e35b9198c4acb2e2093d18918
jbowers19 <jeff.bowers48@gmail.com>
2020-04-26 22:58:23 -0400

    Create cURL.php

---
+++ cURL.php
@@ -0,0 +1,21 @@
+<?php
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, 'http://www.google.com');
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+curl_setopt($ch, CURLOPT_HEADER, 0);
+
+$output = curl_exec($ch);
+
+if ($output === FALSE) {
+    echo "cURL Error " . curl_error($ch);
+}
+
+curl_close($ch);
+print_r($output);
+
+
+?>
